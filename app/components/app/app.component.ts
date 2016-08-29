92df6cec80600cfe0072574df01235a8d67e4c53
jjcarrancia <jjcn1992@hotmail.com>
2016-08-29 15:36:23 +0200

    Project initialized

---
+++ app/components/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  selector: 'angular2-app',
+  template: `
+    <div><h1>Hola Mundo</h1></div>
+  `,
+})
+
+export class AppComponent {
+  constructor()
+  {
+
+  }
+}
